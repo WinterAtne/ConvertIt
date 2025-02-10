68d6b37878212563110a350e10ea12f44320e853
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-02-09 23:13:00 -0700

    Rudimentry Prototype

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 class Main {
+	// Args 0 is the number of units
+	// Args 1 is the unit
+	// Args 3 is the unit to convert to
 	public static void main(String[] Args) {
-		System.out.println("Hello World");
+		if (Args.length < 3) { // Guard
+			System.out.println("Not enought arguments");
+			return;
+		}
+
+		String rawCount = Args[0];
+		String rawUnit = Args[1];
+		String rawConversion = Args[2];
+		
+		Double count = Double.parseDouble(rawCount);
+
+		if ("f" == rawUnit) {
+			System.out.println(count * 12);
+		} else {
+			System.out.println(count / 12);
+		}
 	}
 }
