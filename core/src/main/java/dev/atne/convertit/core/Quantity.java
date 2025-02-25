338318b77cf740070acfc5671b4efcf8b6f7be29
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-02-24 23:37:11 -0700

    Unit type maps are now initialized using json objects

--- core/src/main/java/dev/atne/convertit/core/Quantity.java
+++ core/src/main/java/dev/atne/convertit/core/Quantity.java
@@ -1,31 +1,47 @@
 package dev.atne.convertit.core;
 
 import java.math.BigDecimal;
-import java.util.Map;
-import java.util.Scanner;
-import java.io.InputStream;
+import java.util.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
 
 public final class Quantity {
-	static {
-		// Parse the json resources into maps
-		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-		InputStream is = classloader.getResourceAsStream("units/foot.json");
-		Scanner s = new Scanner(is).useDelimiter("\\A");
-		String result = s.hasNext() ? s.next() : "";
-		s.close();
-		
-	}
-
-	public static final Map<String, String> unitNames = Map.ofEntries(
-		Map.entry("feet",				"foot"),
-		Map.entry("meters",			"meter")
-	);
+	public static final Map<String, String> unitNames = new HashMap<String, String>();
 
 	// Map of all length conversion to meters
-	public static final Map<String, BigDecimal> conversionFactors = Map.ofEntries(
-		Map.entry("meter",		new BigDecimal("1.0")),
-		Map.entry("foot",			new BigDecimal("0.3048"))
-	);
+	public static final Map<String, BigDecimal> conversionFactors = new HashMap<String, BigDecimal>();
+
+	private static final JSONObject loadJSONResource(String name) {
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		InputStream unitsIndexSource = classloader.getResourceAsStream(name + ".json");
+		Scanner scanner = new Scanner(unitsIndexSource).useDelimiter("\\A");
+		String result = scanner.hasNext() ? scanner.next() : "";
+		scanner.close();
+
+		return new JSONObject(result);
+	}
+
+	static {
+		JSONArray unitsIndex = loadJSONResource("units/units_index").getJSONArray("index");
+
+		for (int i = 0; i < unitsIndex.length(); i++) {
+			JSONObject obj = loadJSONResource("units/" + unitsIndex.getString(i));
+			
+			String name = obj.getString("name");
+			JSONArray alternative_names = obj.getJSONArray("alternative_names");
+			unitNames.put(name, name); 
+			for (int k = 0; k < alternative_names.length(); k++) {
+				unitNames.put(alternative_names.getString(k), name); 
+			}
+
+			BigDecimal factor = obj.getBigDecimal("length_scaler");
+			conversionFactors.put(name, factor);
+		}
+	}
+
 
 	// Indicies into the arrays
 	private final short time 			= 0;
