aa4c9367a3060e4dfae3a4596a33caa763ebf9d3
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-03-12 00:31:28 -0600

    Added a few more temporary tests

--- core/src/test/java/dev/atne/convertit/core/QuantityTest.java
+++ core/src/test/java/dev/atne/convertit/core/QuantityTest.java
@@ -11,9 +11,15 @@ class QuantityTest {
 
 	Map<String, Unit> units = Map.ofEntries(
 		Map.entry(
-			"meter/second",
+			"meter2",
 			new Unit(new BigDecimal[] {new BigDecimal("1"), new BigDecimal("1")},
-						new BigDecimal[] {new BigDecimal("1"), new BigDecimal("1")})
+						new BigDecimal[] {new BigDecimal("1"), new BigDecimal("2")})
+		),
+
+		Map.entry(
+			"feet2",
+			new Unit(new BigDecimal[] {new BigDecimal("1"), new BigDecimal("0.3048")},
+						new BigDecimal[] {new BigDecimal("1"), new BigDecimal("2")})
 		),
 
 		Map.entry(
@@ -23,8 +29,14 @@ class QuantityTest {
 		),
 
 		Map.entry(
-			"feet/minute2",
-			new Unit(new BigDecimal[] {new BigDecimal("60"), new BigDecimal("0.3048")},
+			"feet/second2",
+			new Unit(new BigDecimal[] {new BigDecimal("1"), new BigDecimal("0.3048")},
+						new BigDecimal[] {new BigDecimal("-2"), new BigDecimal("1")})
+		),
+
+		Map.entry(
+			"kilometer/hour2",
+			new Unit(new BigDecimal[] {new BigDecimal("3600"), new BigDecimal("1000")},
 						new BigDecimal[] {new BigDecimal("-2"), new BigDecimal("1")})
 		)
 	);
@@ -33,16 +45,47 @@ class QuantityTest {
 
 	@Test
 	void Conversion() {
-		Quantity start = new Quantity(
+		Quantity start;
+		Quantity end;
+		Quantity converted;
+
+		// TEST 1
+		start = new Quantity(
+				new BigDecimal("2.48"),
+				units.get("meter2")
+				);
+		end =  new Quantity(
+				new BigDecimal("26.694497"),
+				units.get("feet2")
+				);
+		converted = start.Convert(units.get("feet2"));
+		System.out.println(converted.getValue().toString());
+		assertTrue(Quantity.IsEqual(converted, end));
+
+		// TEST 2
+		start = new Quantity(
 				BigDecimal.ONE,
 				units.get("meter/second2")
 				);
-		Quantity finalQuantity = new Quantity(
-				new BigDecimal("11811.023"),
-				units.get("feet/minute2")
+		end =  new Quantity(
+				new BigDecimal("12960"),
+				units.get("kilometer/hour2")
 				);
+		converted = start.Convert(units.get("kilometer/hour2"));
+		System.out.println(converted.getValue().toString());
+		assertTrue(Quantity.IsEqual(converted, end));
 		
-		Quantity converted = start.Convert(units.get("feet/minute2"));
-		assertTrue(Quantity.IsEqual(converted, finalQuantity));
+		// TEST 3
+		start = new Quantity(
+				new BigDecimal("2"),
+				units.get("feet/second2")
+				);
+		end =  new Quantity(
+				new BigDecimal("7900.416"),
+				units.get("kilometer/hour2")
+				);
+		converted = start.Convert(units.get("kilometer/hour2"));
+		System.out.println(converted.getValue().toString());
+		assertTrue(Quantity.IsEqual(converted, end));
 	}
 }
